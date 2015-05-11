512282d40de308c93969df625b1a76595744a707
Jason Lipowicz <jlipowicz@hotmail.co.uk>
2015-05-11 17:42:32 +0100

    2 - Tooltips on people menu

--- client.js
+++ client.js
@@ -9,4 +9,14 @@ $(function () {
 		}
 	});
 
+	$('.people ul li[data-hover]').hover(function () {
+		$('.tooltip').remove();
+		$('body').append('<div class="tooltip">' + $(this).find('i.status')[0].outerHTML + $(this).attr('data-hover') + '</div>');
+		t = $(this).position().top + $('.container').position().top + 30;
+		l = $(this).position().left + $('.container').position().left - 408;
+		$('.tooltip').css({ top: t, left: l });
+	}, function () {
+		$('.tooltip').remove();
+	});
+
 });
--- index.php
+++ index.php
@@ -34,22 +34,22 @@
 		<div class="people">
 		<ul>
 
-			<li>
+			<li data-hover="Jason Lipowicz">
 				<i class="status online"></i>
 				<img class="avatar" src="avatar-standard.png" />
 			</li>
 
-			<li>
+			<li data-hover="Jason Lipowicz">
 				<i class="status busy"></i>
 				<img class="avatar" src="avatar-standard.png" />
 			</li>
 
-			<li>
+			<li data-hover="Jason Lipowicz">
 				<i class="status unavailable"></i>
 				<img class="avatar" src="avatar-standard.png" />
 			</li>
 
-			<li>
+			<li data-hover="Jason Lipowicz">
 				<i class="status offline"></i>
 				<img class="avatar" src="avatar-standard.png" />
 			</li>
